85dea7353b0bc092262e01067e46481bda65638a
HuaHenry <henryhua0721@foxmail.com>
2024-10-06 22:52:13 +0800

    finish Generator

--- Generator.py
+++ Generator.py
@@ -7,4 +7,14 @@
 @Desc    :   Linear + Softmax 层实现生成器
 '''
 
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self, dim_model, vocab_size):
+        super(Generator, self).__init__()
+        # 线性变换
+        self.proj = nn.Linear(dim_model, vocab_size)
+
+    def forward(self, x):
+        return nn.functional.log_softmax(self.proj(x), dim=-1)
 
